12c04c5ed7e605471f46bfd599089fabd22a52b1
Vergie Ericson T <vergieericsont@gmail.com>
2016-11-04 00:28:56 +0700

    onBackPressed Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -3,6 +3,7 @@ package id.sch.smktelkom_mlg.learn.intent1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class HasilActivity extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class HasilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -18,5 +20,14 @@ public class HasilActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
